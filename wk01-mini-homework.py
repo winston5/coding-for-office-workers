d7cc3f0907e4b648535e65b8509fd9253aa01ab3
unknown <winston5@naver.com>
2018-08-11 22:16:31 +0900

    Add wk01-mini-homework.py > -new file>

---
+++ wk01-mini-homework.py
@@ -0,0 +1,21 @@
+import random
+list = [1, 2, 3, 4, 5]
+print(random.choice(list))
+
+import random
+list = [1, 2, 3, 4, 5]
+print(random.sample(list,3))
+
+import random
+print(random.randint(1,5))
+#
+# import random
+# flower = ["rose", "lily", "lavender"]
+# print(random.choice(flower))
+#
+# continent = input("If you choose your fav continent out of Africa, Asia, Europe and America, I will recommend you a country to visit?")
+# Africa = ["Zimbabwe", "Botswana", "South Africa", "Lethoso"]
+# Asia = ["Korea", "China", "Japan"]
+# Europe = ["Germany", "France", "Switzerland", "France", "Italy"]
+# America = ["USA", "Mexico", "Canada"]
+# print(random.choice(continent))
