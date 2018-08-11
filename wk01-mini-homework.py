8c736f693ed12ac0de9972ef9862bcbbf50e19da
unknown <winston5@naver.com>
2018-08-11 22:59:49 +0900

    Added exercises using randint, countries[]

--- wk01-mini-homework.py
+++ wk01-mini-homework.py
@@ -8,14 +8,31 @@ print(random.sample(list,3))
 
 import random
 print(random.randint(1,5))
-#
-# import random
-# flower = ["rose", "lily", "lavender"]
-# print(random.choice(flower))
-#
-# continent = input("If you choose your fav continent out of Africa, Asia, Europe and America, I will recommend you a country to visit?")
-# Africa = ["Zimbabwe", "Botswana", "South Africa", "Lethoso"]
-# Asia = ["Korea", "China", "Japan"]
-# Europe = ["Germany", "France", "Switzerland", "France", "Italy"]
-# America = ["USA", "Mexico", "Canada"]
-# print(random.choice(continent))
+
+import random
+print(random.randint(list[0],list[4]))
+
+import random
+from random import randint
+countries = ["Korea", "Japan", "China", "USA", "Singapore"]
+rand_index = randint(0,5)
+print(countries[rand_index])
+
+print(type(countries[rand_index]))
+print(type(random.sample(countries,1)))
+print(type(random.choice(countries)))
+
+import random
+flower = ["rose", "lily", "lavender"]
+print(random.choice(flower))
+
+import random
+Africa = ["Zimbabwe", "Botswana", "South Africa", "Lethoso"]
+Asia = ["Korea", "China", "Japan","Thailand"]
+Europe = ["Germany", "France", "Switzerland", "France"]
+America = ["USA", "Mexico", "Canada","Argentina"]
+
+def whereto(continent):
+    print(random.choice(continent))
+
+whereto(Africa)
