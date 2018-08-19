eee1a10f7c4114457da1c06cba1128aaabc9a39f
unknown <winston5@naver.com>
2018-08-19 23:16:34 +0900

    Add week-02-django project

--- week-02-django/test.py
+++
@@ -1,6 +0,0 @@
-echo "# test" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/winston5/coding-for-office-workers/test.git
-git push -u origin master
