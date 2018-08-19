46b2624455500ee332e113dc615cd39c74e63d10
unknown <winston5@naver.com>
2018-08-19 18:42:45 +0900

    New folder

---
+++ week-02-django/test.py
@@ -0,0 +1,6 @@
+echo "# test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/winston5/coding-for-office-workers/test.git
+git push -u origin master
